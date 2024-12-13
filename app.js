905b8d6fb82d99c5ad874f5f3f72874294d34bc9
JensonAquino <jensonaquino2622@gmail.com>
2024-12-13 03:18:20 +0100

    feat: middleware per gli errori aggiunto

--- app.js
+++ app.js
@@ -4,18 +4,26 @@ const port = 3003;
 const postsRouter = require("./routers/posts")
 //const posts = require("./data") importato nel router
 
-app.use(express.static("public"))
+//middlewere globale a livello globale
+const handleError = require("./middlewares/handleError");
 
 //includere il body-parser per leggere il req.body sempre prima del router
 app.use(express.json());
+
+app.use(express.static("public"))
+
+
+//includo tutte le rotte dei post con prefisso posts
+app.use("/posts", postsRouter)
 
 app.get("/", (req, res) => {
     res.json({
         message: "Server del mio blog"
     })
 });
-//includo tutte le rotte dei post con prefisso posts
-app.use("/posts", postsRouter)
+
+//eseguito alla fine delle rotte perche deve essere esguito dopo
+app.use(handleError)
 
 app.listen(port, () => {
     console.log("listener on");
